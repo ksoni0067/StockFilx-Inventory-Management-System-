5e70a1fc33a6559c3f63d9ea7058a04e1ff4874b
keshav <keshav1787.be22@chitkara.edu.in>
2024-09-17 19:22:17 +0530

    login sign up working using local storage

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
 const main=document.querySelector('.main');
-const signin=document.querySelectorAll(".signin");
-const login=document.querySelectorAll('.login');
 const log=document.querySelector('.log');
 const sig=document.querySelector('.sig');
-signin.forEach((e)=>{e.addEventListener('click',()=>{
-    
+document.getElementById('create').addEventListener('click', signUp);
+document.querySelector('#Logi').addEventListener('click', logIn);
+function signUp(event) {
+    event.preventDefault();
+    const businessName = document.getElementById('Bname').value;
+    const gstin = document.getElementById('gstin').value;
+    const ownerName = document.getElementById('own').value;
+    const password = document.getElementById('set').value;
+    const confirmPassword = document.getElementById('confi').value;
+    if(businessName=='' || gstin =='' || ownerName==''|| password=='' ){
+        alert('Please Fill All Feilds');
+        return ;
+    }
+    if( gstin.length<8 || password.length<8){
+        alert('Invalid Business ID or Password');
+        return ;
+    }
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+  
+    const userDetails = {
+      businessName: businessName,
+      gstin: gstin,
+      ownerName: ownerName,
+      password: password,
+    };
+    document.querySelector('.formsig').reset();
+    localStorage.setItem(gstin, JSON.stringify(userDetails));
+  
+    alert('Account created successfully!');
+}
+  
+function logIn(event) {
+    event.preventDefault(); 
+    const enteredBusinessId = document.getElementById('BID').value;
+    const enteredPassword = document.getElementById('logPass').value;
+    const storedUser = JSON.parse(localStorage.getItem(enteredBusinessId));
+    if (storedUser &&  storedUser.password === enteredPassword) {
+        alert('Login successful!');
+        localStorage.setItem("BID",enteredBusinessId);
+        document.querySelector('.formlog').reset();
+        window.location.href = 'dashBoard.html';
+
+    } else {
+        alert('Invalid Business ID or Password');
+    }
+}
+document.querySelectorAll(".signin").forEach((e)=>{e.addEventListener('click',()=>{
     main.style.display='none';
     log.style.display='none';
     sig.style.display='flex';
     e.preventDefault(); 
 })});
-login.forEach((e)=>{e.addEventListener('click',()=>{
+document.querySelectorAll('.login').forEach((e)=>{e.addEventListener('click',()=>{
     main.style.display='none';
     sig.style.display='none';
     log.style.display='flex';
